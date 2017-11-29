205a88a329e62f36a7c7a3353b6f01e1cca2df3c
pengzhen <529629354@qq.com>
2017-11-29 19:12:38 +0800

    haha

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import CoolCat from './CoolCat';
+
+
+export default class App extends Component {
+
+  render() {
+
+    return (
+      <main>
+        1231231
+        <CoolCat />
+      </main>
+    );
+  }
+}
